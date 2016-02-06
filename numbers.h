caec5e23f2140a6435e80a5e07ad8163538ee743
Michael Jay Braga <michaelbraga.uplb@gmail.com>
2016-02-06 21:21:57 +0800

    starting point

---
+++ numbers.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void numToWords(/* arguments */) {
+	/* code */
+}
+
+int wordsToNum(/* arguments */) {
+	/* code */
+}
+
+char * wordsToCurrency(/* arguments */) {
+	/* code */
+}
+
+void numberDelimited(/* arguments */) {
+	/* code */
+}
