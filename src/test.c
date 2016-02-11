e7235785d383e62f9631cb822f836f353160a0e6
Michael Jay Braga <majcbraga@gmail.com>
2016-02-11 22:45:29 +0800

    Test

--- src/test.c
+++ src/test.c
@@ -2,14 +2,19 @@
 /*
 	For testing purposes only
 */
+
 int main(int argc, char const *argv[]) {
 
 	int x = -1;
-	do {
-		scanf("%d", &x);
-		numToWords(x);
-		putchar('\n');
-	} while(x!=-1);
+	char something[80];
+	char * token;
+	char s[2] = " ";
+	char currr[4] = "PHP";
+	while (1) {
+		fgets(something, 79, stdin);
+		something[strlen(something)-1] = '\0';
+		printf("===%s\n", wordsToCurrency (something, currr));
 
+	}
 	return 0;
 }
